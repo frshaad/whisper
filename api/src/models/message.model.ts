d82822a708f6ae0704c14c349a895eff8276daa3
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-04-25 15:25:48 +0330

    refactor(api): restructure message schema

--- api/src/models/message.model.ts
+++ api/src/models/message.model.ts
@@ -1,13 +1,30 @@
-import { model, Schema } from 'mongoose'
+import { type InferSchemaType, model, Schema } from 'mongoose'
 
 const messageSchema = new Schema(
   {
-    senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    receiverId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    text: { type: String },
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+    text: { type: String, trim: true, maxlength: 2000 },
     image: { type: String },
+    readStatus: { type: Boolean, default: false },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 )
 
+messageSchema.index({ senderId: 1, receiverId: 1, createdAt: -1 })
+
 export const Message = model('Message', messageSchema)
+export type MessageDoc = InferSchemaType<typeof messageSchema>
