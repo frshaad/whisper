e609ccbb9232ebf307568194f14fe6afe2b299c0
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-04-27 15:02:24 +0330

    feat(client): implement routing and add authentication pages with forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
-import ThemeProvider from '@/components/theme-provider'
+import { Route, Routes } from 'react-router'
 
-export default function App({ children }: React.PropsWithChildren) {
+import ThemeProvider from '@/components/theme-provider'
+import AuthLayout from '@/pages/auth-layout'
+import Home from '@/pages/home'
+import Login from '@/pages/login'
+import Profile from '@/pages/profile'
+import Settings from '@/pages/settings'
+import SignUp from '@/pages/signup'
+
+export default function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      {children}
+      <div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<Profile />} />
+
+          <Route element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </div>
     </ThemeProvider>
   )
 }
---
+++ client/src/components/signup-form.tsx
@@ -0,0 +1,48 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+
+export function SignUpForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'form'>) {
+  return (
+    <form className={cn('flex flex-col gap-6', className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Create a new account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your username below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="fullname">Full Name</Label>
+          <Input id="fullname" type="text" placeholder="John Doe" required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" type="text" placeholder="johndoe" required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input id="confirmPassword" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Sign Up
+        </Button>
+      </div>
+
+      <div className="text-center text-sm">
+        ALready have an account?{' '}
+        <a href="/login" className="underline underline-offset-4">
+          Log In
+        </a>
+      </div>
+    </form>
+  )
+}
